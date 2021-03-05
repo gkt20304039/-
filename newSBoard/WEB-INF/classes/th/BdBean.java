eabb6bbd6d82dbc83a57170583ed8b3975a940f4
Mizuyo-kan <73629674+Mizuyo-kan@users.noreply.github.com>
2021-03-05 11:46:40 +0900

    Delete newSBoard directory

--- newSBoard/WEB-INF/classes/th/BdBean.java
+++
@@ -1,9 +0,0 @@
-/* package th;
-
-public class BdBean extends UserBean {
-	public void setAllContents(String id, int num, String name, String date, String text){
-		setId(id);
-		setName(name);
-		setText(text);
-	}
-} */
--- newSBoard/WEB-INF/classes/th/ThBean.java
+++
@@ -1,9 +0,0 @@
-package th;
-
-public class ThBean extends UserBean {
-	public void setAllContents(String id, String name, String date){
-		setId(id);
-		setName(name);
-		setDate(date);
-	}
-}
