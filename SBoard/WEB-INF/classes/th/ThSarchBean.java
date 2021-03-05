db5a71c2db280b8a37ad6c49a59ff32d8636b744
Mizuyo-kan <73629674+Mizuyo-kan@users.noreply.github.com>
2021-03-05 11:35:40 +0900

    Delete ThSarchBean.java

--- SBoard/WEB-INF/classes/th/ThSarchBean.java
+++
@@ -1,9 +0,0 @@
-package th;
-
-public class ThSarchBean extends UserBean {
-	public void setAllContents(String id, String name, String date){
-		setId(id);
-		setName(name);
-		setDate(date);
-	}
-}
